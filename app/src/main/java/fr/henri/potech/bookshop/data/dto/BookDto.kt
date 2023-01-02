0fd198d2bba2d33ecadcacb0171d7948c0e972fe
Simon Sassi <2765875+maxerbox@users.noreply.github.com>
2023-01-02 16:53:36 +0100

    feat: finish implementing dto for api

--- app/src/main/java/fr/henri/potech/bookshop/data/dto/BookDto.kt
+++ app/src/main/java/fr/henri/potech/bookshop/data/dto/BookDto.kt
@@ -3,11 +3,11 @@ package fr.henri.potech.bookshop.data.dto
 import com.google.gson.annotations.SerializedName
 
 
-data class BooksDto(
+data class BookDto(
 
     @SerializedName("isbn") var isbn: String,
     @SerializedName("title") var title: String,
-    @SerializedName("price") var price: Int,
+    @SerializedName("price") var price: Double,
     @SerializedName("cover") var cover: String,
     @SerializedName("synopsis") var synopsis: ArrayList<String> = arrayListOf()
 
