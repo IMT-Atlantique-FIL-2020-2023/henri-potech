11982ddeeb6ec0b52ff0b3e7f0d78344cc269127
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2023-01-02 16:53:36 +0100

    feat: finish implementing dto for api

--- app/src/main/java/fr/henri/potech/bookshop/data/dto/CommercialOffers.kt
+++ app/src/main/java/fr/henri/potech/bookshop/data/dto/CommercialOffers.kt
@@ -1,14 +1,33 @@
 package fr.henri.potech.bookshop.data.dto
 
 import com.google.gson.annotations.SerializedName
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory
 
 data class CommercialOffersDto(
 
-    @SerializedName("offers") var offers: ArrayList<OffersDto> = arrayListOf()
+    @SerializedName("offers") var offers: ArrayList<OfferDto> = arrayListOf()
 
 )
 
-data class OffersDto(
+
+data class OfferDto(
     @SerializedName("type") var type: String,
-    @SerializedName("value") var value: Int
 )
+
+sealed class OfferType {
+    data class Percentage(@SerializedName("value") val value: Double) : OfferType()
+    data class Minus(@SerializedName("value") val value: Double) : OfferType()
+    data class Slice(
+        @SerializedName("value") val value: Double,
+        @SerializedName("sliceValue") val sliceValue: Double
+    ) : OfferType()
+
+    companion object {
+        val runtimeTypeAdapterFactory = RuntimeTypeAdapterFactory
+            .of(OfferType::class.java, "type")
+            .registerSubtype(Percentage::class.java, "percentage")
+            .registerSubtype(Minus::class.java, "minus")
+            .registerSubtype(Slice::class.java, "slice")
+    }
+
+}
