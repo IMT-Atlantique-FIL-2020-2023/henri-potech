62dd441dfaa21d18d9c1316d12349401f9f09876
Simon Sassi <2765875+maxerbox@users.noreply.github.com>
2023-01-04 22:37:41 +0100

    feat: add compute best offer and book fetching

--- app/src/main/java/fr/henri/potech/bookshop/domain/Cart.kt
+++ app/src/main/java/fr/henri/potech/bookshop/domain/Cart.kt
@@ -1,7 +1,30 @@
 package fr.henri.potech.bookshop.domain
 
+import fr.henri.potech.bookshop.data.dto.OfferTypeDTO
+import fr.henri.potech.bookshop.data.remote.HenriPotierApi
+import fr.henri.potech.bookshop.data.remote.ListToStringJoin
+
 data class Cart(
     var books: List<Book>,
-    var availableOffers: List<Offer>,
-    var bestOffer: Offer?
-)
+
+    ) {
+    fun total(): Double {
+        return books.sumOf { it.price }
+    }
+
+    suspend fun computeTotalWithOffer(): Double {
+        val commercialOffers =
+            HenriPotierApi.client.getCommercialOffers(ListToStringJoin(books.map { it.isbn }))
+        val availableOffers = commercialOffers.offers.map { offer ->
+            when (offer) {
+                is OfferTypeDTO.Percentage -> Percentage(offer.value)
+                is OfferTypeDTO.Minus -> Minus(offer.value)
+                is OfferTypeDTO.Slice -> Slice(offer.sliceValue, offer.value)
+            }
+        }
+
+        return availableOffers.map { it.apply(total()) }.minOrNull() ?: total()
+
+    }
+
+}
--- app/src/main/java/fr/henri/potech/bookshop/domain/Library.kt
+++ app/src/main/java/fr/henri/potech/bookshop/domain/Library.kt
@@ -1,5 +1,21 @@
 package fr.henri.potech.bookshop.domain
 
-data class Library(
-    var books: List<Book>
-)
+import fr.henri.potech.bookshop.data.remote.HenriPotierApi
+import java.net.URL
+
+data class Library(val books: List<Book>) {
+    companion object {
+        suspend fun fetchBooks(): Library {
+            return Library(HenriPotierApi.client.getBooks().map { book ->
+                Book(
+                    coverUrl = URL(book.cover),
+                    title = book.title,
+                    isbn = book.isbn,
+                    synopsis = book.synopsis.joinToString(""),
+                    price = book.price
+                )
+            })
+        }
+    }
+}
+
